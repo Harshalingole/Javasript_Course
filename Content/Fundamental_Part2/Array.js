8ffeb2ffaadf43ec8dc4194f8675290be1c8bb31
Harshalingole <harshal.ingole21@gmail.com>
2022-01-13 18:34:07 +0530

    Array in content

---
+++ Content/Fundamental_Part2/Array.js
@@ -0,0 +1,14 @@
+//Array Method
+const first = [1,2,"3","4"];
+//To Remove
+const a = first.pop(); //last element --Pop
+a
+first
+console.log(typeof a)
+
+const b = first.shift() //fist element --shift
+b
+first
+
+
+
