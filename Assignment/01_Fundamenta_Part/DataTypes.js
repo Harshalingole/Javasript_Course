6563fc5070ca148141094e31b2d65a8743c77a7b
Harshalingole <harshal.ingole21@gmail.com>
2022-01-21 23:41:07 +0530

    Datatypes

---
+++ Assignment/01_Fundamenta_Part/DataTypes.js
@@ -0,0 +1,17 @@
+/* 1. Declare a variable called 'isIsland' and set its value according to your
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console
+*/
+let country,continent,population;
+country = "India";
+continent = "Asia";
+population = 1.38;
+let isIsMumbai = true;
+let language;
+console.log(typeof isIsMumbai);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
