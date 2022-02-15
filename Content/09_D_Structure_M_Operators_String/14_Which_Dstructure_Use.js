102a8f3ce491eb6b450114193fcae422ccd5cd83
Harshalingole <harshal.ingole21@gmail.com>
2022-02-15 21:28:43 +0530

    Destructure

---
+++ Content/09_D_Structure_M_Operators_String/14_Which_Dstructure_Use.js
@@ -0,0 +1,46 @@
+'use strict';
+/*
+Source Of Data
+1) Progaram Itself
+2) From the UI
+3) From External sources(API)
+
+*/
+/* Collection Of Data
+   Data sturcuture
+   a) Array --> Simple List
+   b) Object or Maps --> Key value Pairs
+*/
+// -----ARRAYS VS. SETS----------
+
+// [1] ARRAYS
+/*
+ a) Need in Orderd
+ b) Might Contain Duplicate Values
+ c) Need To Manupulate Data
+*/
+// [2] SETS
+/*    Sets are not meant to  raplace array they are meant to compliment array
+a) With Unique Values
+b) High performance important
+c) Remove duplicates from arays
+*/
+
+// -----OBJECTS VS. MAPS----------
+
+// [1] OBJECTS
+/*
+ a)Traditional key/value store
+ b) Easier to write and access values
+   with . and []
+ c) Values as function(methods)
+ d) Working with JSON(can convert to map)
+*/
+
+// [1] MAPS
+/*
+ a)Better Performance
+ b) Key can have any data type
+ c) Easy to iterate
+ d) Easy to compute size
+*/
