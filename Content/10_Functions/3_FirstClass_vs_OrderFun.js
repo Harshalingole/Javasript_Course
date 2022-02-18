e60a922d31d582dd0bf4685fd9929ebce9208f34
Harshalingole <harshal.ingole21@gmail.com>
2022-02-18 21:20:57 +0530

    First Class vs OrderFun

---
+++ Content/10_Functions/3_FirstClass_vs_OrderFun.js
@@ -0,0 +1,28 @@
+//First Class Funtion Vs Higher Order Funtion
+
+/* 1) FIRST CLASS FUNTION
+a) Javascript treats funtion s first-class citizens
+b) This means that funtion are simpy values
+c) Funtions are just another "type" of object
+*/
+/* 
+Examples OF first Class Funtion
+a) Store funtion in variable or properties
+
+b) Pass funtion as arguments to other funtions:
+
+c) Return funtion FROM funtions:
+
+d) Call methods on funtions:
+*/
+
+/* 2) HIGHER ORDER FUNTION
+a) that recives another funtion as an argument, that returns a new funtion or both
+
+b) Only possible bcz of first class funtion
+ 
+*/
+
+/*
+
+*/
